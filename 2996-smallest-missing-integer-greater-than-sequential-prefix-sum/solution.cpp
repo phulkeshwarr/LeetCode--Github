a2494f6eb189ec7a8d05afca12cb3040dc8c1248
Phulkeshwar Mahto <phulkeshwar.e@gmail.com>
2026-08-23 22:46:27 +0530

    [Easy] Solved: Smallest Missing Integer Greater Than Sequential Prefix Sum (Runtime: N/Ams)

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/solution.cpp
@@ -0,0 +1,10 @@
+break; // Stop at the first non-sequential element
+            } else {
+            if (nums[i] == nums[i - 1] + 1) {
+                prefixSum += nums[i];
+        for (int i = 1; i < nums.size(); i++) {
+        // 1. Calculate the sum of the longest sequential prefix
+        int prefixSum = nums[0];
+    int missingInteger(vector<int>& nums) {
+public:
+class Solution {
